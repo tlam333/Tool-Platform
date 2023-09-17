c5e454c7dab75c7f6225e7165c6e8b8551338102
synk-nagendra <nagendrayadav075@gmail.com>
2023-09-17 20:38:21 +1000

    updated list tools page

--- src/app/list-tools/page.tsx
+++ src/app/list-tools/page.tsx
@@ -2,17 +2,14 @@ import { Metadata } from "next";
 import Script from "next/script";
 
 export const metadata: Metadata = {
-  other: { "og:url": "https://www.nearbytools.com.au/list-tools/" },
+  other: { "og:url": "https://sharetools.lpages.co/list-tools/" },
 };
 
 export default async function ListTools() {
   return (
     <>
-      <link
-        rel="opengraph"
-        href="https://www.nearbytools.com.au/list-tools//"
-      />
-      <Script src="//www.nearbytools.com.au/_/js/list-tools/"></Script>
+      <link rel="opengraph" href="https://sharetools.lpages.co/list-tools/" />
+      <Script src="//sharetools.lpages.co/_/js/list-tools/"></Script>
     </>
   );
 }
