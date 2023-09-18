68b2741dd64d9028156776b673ff35c162c46d0e
Nagendra Yadav <nagendra@nearbytools.com.au>
2023-09-18 12:59:46 +1000

    Added sendgrid integration details

--- src/app/api/booking/route.ts
+++ src/app/api/booking/route.ts
@@ -1,6 +1,8 @@
 const apiURL = process.env.AIRTABLE_API_URL;
 const baseId = process.env.BASE_ID;
 import { NextResponse } from "next/server";
+import sgMail from "@sendgrid/mail";
+
 export async function POST(request: Request) {
   var url = `${apiURL}/${baseId}/Bookings`;
 
@@ -12,6 +14,7 @@ export async function POST(request: Request) {
     suburb,
     postCode,
     toolId,
+    toolName,
     ownerId,
     startDate,
     endDate,
@@ -75,7 +78,43 @@ export async function POST(request: Request) {
     .catch((err) => console.error(err));
 
   const records = await res.records;
-  //console.log(records);
+
+  /** Send email using sendgrid */
+  /**
+   * parameters -
+   * email
+   * firstName
+   * fullName
+   * city tool located in
+   * Template ID: d-80a6f09b8373499c8ef58d5dfa0d5c1d (booking request sent)
+   */
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY as string);
+  // Define the dynamic variables
+  const dynamicTemplateData = {
+    first_name: firstName,
+    full_name: firstName + " " + lastName,
+    city: suburb,
+    tool_name: toolName,
+  };
+  const templateId = "d-80a6f09b8373499c8ef58d5dfa0d5c1d";
+
+  // Create the email message
+  const msg = {
+    to: [{ email: email, name: firstName + " " + lastName }],
+    from: { email: "will@nearbytools.com.au", name: "Will @ Nearby Tools" },
+    templateId,
+    dynamic_template_data: dynamicTemplateData,
+  };
+
+  await sgMail
+    .send(msg)
+    .then(() => {
+      console.log("Email sent");
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+  /** send email ends here */
 
   return NextResponse.json(records);
 }
