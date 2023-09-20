eef64c254fe00b60fecd49b977b035ca627d24b1
Nagendra Yadav <nagendra@nearbytools.com.au>
2023-09-21 08:37:37 +1000

    fixed schema issue for json-LD

--- src/app/(cms)/faq/page.tsx
+++ src/app/(cms)/faq/page.tsx
@@ -35,10 +35,10 @@ export default async function Faq() {
         (item: FAQ) =>
           `{
         "@type": "Question",
-        "name": ${item.question},
+        "name": "${item.question}",
         "acceptedAnswer": {
             "@type": "Answer",
-            "text": <p>${item.answer}</p>,
+            "text": "<p>${item.answer}</p>",
         }
     }`
       );
