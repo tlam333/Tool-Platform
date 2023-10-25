2a3a57099e4913e12e24e8547eed4bde02b5bec6
Nagendra Yadav <nagendra@nearbytools.com.au>
2023-10-25 17:20:45 +1100

    rollback SSG on product page

--- src/app/(tools)/for-hire/[id]/page.tsx
+++ src/app/(tools)/for-hire/[id]/page.tsx
@@ -209,11 +209,11 @@ export default async function ToolDetailsPage({
 }
 
 //this portion forces startic generation of all tools pages
-export async function generateStaticParams() {
-  const toolsPage: Promise<ToolsPage> = getAllTools({ limit: "100" });
-  const { tools, total, nextPage, message } = await toolsPage;
+// export async function generateStaticParams() {
+//   const toolsPage: Promise<ToolsPage> = getAllTools({ limit: "100" });
+//   const { tools, total, nextPage, message } = await toolsPage;
 
-  return tools.map((tool) => {
-    id: tool.id;
-  });
-}
+//   return tools.map((tool) => {
+//     id: tool.id;
+//   });
+// }
